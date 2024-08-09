bb64d6f21c69c8e9b10fdbe2431ca4f737863f8d
Chebel405 <anne.chebel@gmail.com>
2024-08-09 14:59:56 +0200

    Ajout de composants

--- cookchef/src/App.jsx
+++ cookchef/src/App.jsx
@@ -1,8 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import styles from './App.module.scss';
+
 function App() {
   return (
 
-    <div>
-      <h1>Cookchef</h1>
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Footer />
+      <Content />
     </div>
 
   )
