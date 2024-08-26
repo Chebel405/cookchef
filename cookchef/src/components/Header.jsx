4343795d91205f8c628846467f7ae1b9beef21f4
Chebel405 <anne.chebel@gmail.com>
2024-08-26 18:06:21 +0200

    Retirer le menu du header en responsive

--- cookchef/src/components/Header.jsx
+++ cookchef/src/components/Header.jsx
@@ -4,17 +4,18 @@ import cookchef from "../assets/images/cook-chef-logo.jpg";
 function Header() {
     return (
         <header className={`${styles.header} d-flex flex-row align-items-center`}>
-            <i className="fa-solid fa-bars mr-15"></i>
+
             <div className="flex-fill">
                 <img src={cookchef} alt="logo cookchef" />
             </div>
-            <ul>
+            <ul className={styles.headerList}>
                 <button className="mr-5 btn btn-reverse-primary">
-                    <i className="fa-solid fa-basket-shopping mr-5"></i>
-                    <span>panier</span>
+                    <i className="fa-solid fa-heart mr-5"></i>
+                    <span>Wishlist</span>
                 </button>
                 <button className="btn btn-primary">connexion</button>
             </ul>
+            <i className={`fa-solid fa-bars ${styles.headerXs}`}></i>
         </header>
     );
 }
